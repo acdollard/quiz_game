2b60660794ceee0023549f9d0899ca898d44c741
Alex Dollard <alexdollard141@gmail.com>
2019-11-12 17:41:26 -0600

    "added questions"

---
+++ questions.js
@@ -0,0 +1,18 @@
+var questions = [
+    {
+        title: "Chekov's _____ states that if something is present in the story, it should be used.",
+        choices: ["gun", "harpsichord", "labrador", "potato"],
+        answer: "gun",
+    }
+    {
+        title: "All happy families are alike, every unhappy family is unhappy in it's own way- is a quote from:",
+        choices: ["Dostoyevsky", "Hemmingway", "Tolstoy", "Joyce"],
+        answer: "Tolstoy",
+    }
+    {
+        title: "This novel was voted the greatest novel of all time in 2002 by a survey of 100 authors",
+        choices: ["Crime and Punishment", "The Old Man and the Sea", "Hamlet", "Don Quixote"],
+        answer: "Don Quixote",
+    }
+   
+]
