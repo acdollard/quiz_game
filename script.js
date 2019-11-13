d0a57efb5955d1b1f6cb1bd4b0fb303c3639c477
Alex Dollard <alexdollard141@gmail.com>
2019-11-13 16:07:43 -0600

    "added comments for planning and more questions"

---
+++ script.js
@@ -0,0 +1,13 @@
+//title text.content = title of questions
+//<unordered list> with 4 buttons
+//button text.content is questions.choices
+
+//click button, appears the first in a list of arrays (change text content of div)
+//move through an object of arrays per each click
+//on click, values disappear and are replaced by next item 
+
+//set interval timer on click
+//if timer runs out, show high score 
+
+//scores are increased by adding right answers and answering in a shorter amount of time (if functions for 
+// correct answer, and for submitting within a set increment)
