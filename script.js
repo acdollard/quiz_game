34dfd5f0f788d2c6fda2f2982113c0525a44511a
Alex Dollard <alexdollard141@gmail.com>
2019-11-13 19:00:30 -0600

    added functionality to create and append question 1 + choices onClick
    on clicking the start button, the user now sees several newly appended divs containing the title and choices elements of the "questions" array

--- script.js
+++ script.js
@@ -24,13 +24,56 @@ var Food = document.getElementById("food");
 //on click of start button, change text content of main div to first question w/ buttons 
 
 startButtonEl.addEventListener("click", function(){
-  var newDiv = document.createElement("div");
-  newDiv.textContent = questions[0].answer; 
-  mainDiv.appendChild(newDiv);
-  Food.textContent = "";
-  startButtonEl.parentNode.removeChild(startButtonEl);
-  
+
+    startButtonEl.parentNode.removeChild(startButtonEl);
+    Food.textContent = "";
+    renderQuestions()
 })
+
+
+
+
+function renderQuestions(){
+    
+ var titleDiv= document.createElement("div")
+ titleDiv.textContent = questions[0].title;
+ mainDiv.appendChild(titleDiv); 
+
+
+  var li1 = document.createElement("div");
+  var button1 = document.createElement("button");
+  button1.textContent = questions[0].choices[0];
+  mainDiv.appendChild(li1);
+  mainDiv.appendChild(button1);
+
+
+  var li2 = document.createElement("div");
+  var button2 = document.createElement("button");
+  button2.textContent = questions[0].choices[1];
+  mainDiv.appendChild(li2);
+  mainDiv.appendChild(button2); 
+
+  var li3 = document.createElement("div");
+  var button3 = document.createElement("button");  
+  button3.textContent = questions[0].choices[2]; 
+  mainDiv.appendChild(li3); 
+  mainDiv.appendChild(button3); 
+
+  var li4 = document.createElement("div");
+  var button4 = document.createElement('button');
+  button4.textContent = questions[0].choices[3]; 
+  mainDiv.appendChild(li4); 
+  mainDiv.li4.appendChild(button4); 
+  }
+  
+
+
+
+Food.setAttribute("style", "text-size:25px", "text-align:center")
+
+
+
+//one function to render questions, another to change the data-index of the elements
 
 
 
