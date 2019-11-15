ee8791a1a162fed1657290a27d8696de4baea995
Alex Dollard <alexdollard141@gmail.com>
2019-11-15 14:12:20 -0600

    name&score are added to local storage as object

--- end_screen.js
+++ end_screen.js
@@ -1,10 +1,34 @@
-var tryAgain = document.getElementById("tryAgainBtn"); 
-var mostRecentScore = localStorage.getItem("score")
-scoreDivEl = document.getElementById("scoreDiv")
-
+let tryAgain = document.getElementById("tryAgainBtn"); 
+let scoreDivEl = document.getElementById("scoreDiv")
+let nameInputEl = document.getElementById("nameInput")
+let submitScoreEl = document.getElementById("sumbitScoreBtn")
+let highscore = JSON.parse(localStorage.getItem("highscore")); 
+let mostRecentScore = localStorage.getItem("score")
 scoreDivEl.textContent = "Your score: " + mostRecentScore;
 
 
 tryAgain.addEventListener("click", function(){
     return window.location.assign("quiz_game.html");
 });
+
+
+function saveHighScores(){
+    let score = {
+        score: mostRecentScore,
+        name: nameInputEl.value
+    }
+    highscore.push(score); 
+    localStorage.setItem("highscore", JSON.stringify(highscore));
+
+    console.log(highscore)
+
+}   
+
+submitScoreEl.addEventListener("click", function(event){
+    event.preventDefault()
+    saveHighScores();
+
+})
+
+
+
