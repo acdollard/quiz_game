d3bd69ed982f14a9f76e39be2b6b0d1ba4fbb2c8
Alex Dollard <alexdollard141@gmail.com>
2019-11-15 15:25:46 -0600

    "fixed minor but, still can't get high score to render"

--- end_screen.js
+++ end_screen.js
@@ -2,10 +2,13 @@ let tryAgain = document.getElementById("tryAgainBtn");
 let scoreDivEl = document.getElementById("scoreDiv")
 let nameInputEl = document.getElementById("nameInput")
 let submitScoreEl = document.getElementById("sumbitScoreBtn")
-let highscore = JSON.parse(localStorage.getItem("highscore")); 
 let mostRecentScore = localStorage.getItem("score")
 scoreDivEl.textContent = "Your score: " + mostRecentScore;
+let viewHighScores = document.getElementById("highScores");
+let highscore = JSON.parse(localStorage.getItem("highscore")); 
 
+
+ 
 
 tryAgain.addEventListener("click", function(){
     return window.location.assign("quiz_game.html");
@@ -17,18 +20,28 @@ function saveHighScores(){
         score: mostRecentScore,
         name: nameInputEl.value
     }
+    debugger;
     highscore.push(score); 
-    localStorage.setItem("highscore", JSON.stringify(highscore));
+    localStorage.setItem("highscore", JSON.stringify(highscore))
+};
 
-    console.log(highscore)
+console.log()  
 
-}   
 
-submitScoreEl.addEventListener("click", function(event){
-    event.preventDefault()
+
+submitScoreEl.addEventListener("click", function(){
     saveHighScores();
-
 })
+
+// viewHighScores.addEventListener("click", function(){
+//     let scoreDiv = document.createElement("div");
+//     scoreDiv.innerText = JSON.parse(localStorage.getItem("highscore"));
+//     document.appendChild.scoreDiv;
+
+// })
+
+
+
 
 
 
