b4d97fdffb85b9dbdf35cdf9964fa527a6f6c58a
Alex Dollard <alexdollard141@gmail.com>
2020-06-10 16:17:50 -0500

    added name + score validation to highscores page

--- high_score.js
+++ high_score.js
@@ -9,16 +9,22 @@ tryAgain.addEventListener("click", function(){
     return window.location.assign("quiz_game.html");
 });
 
-
+//looop through list of players and scores, retrieved from local storage
 for (let i=0; i < highscores.length; i++){
-  row= document.createElement("tr");
-  tableBody.appendChild(row);
-  scoreData = document.createElement("th");
-  nameData = document.createElement("th");
-  scoreData.textContent = highscores[i].score;
-  nameData.textContent = highscores[i].name;
-  row.appendChild(scoreData);
-  row.appendChild(nameData);
+
+  //check to make sure each entry has a name and score
+  if(highscores[i].score !== null && highscores[i].name !== "") {
+    row= document.createElement("tr");
+    tableBody.appendChild(row);
+    scoreData = document.createElement("th");
+    nameData = document.createElement("th");
+    scoreData.textContent = highscores[i].score;
+    nameData.textContent = highscores[i].name;
+    row.appendChild(scoreData);
+    row.appendChild(nameData);
+
+
+  }
 
 
 }
